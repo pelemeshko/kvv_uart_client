a0aec71e63106e602e9f977ecef8360d3f531b5c
Anatolii <pelemeshko55@gmail.com>
2022-01-10 18:33:59 +0700

    0.4 Logging data

--- log_file.py
+++ log_file.py
@@ -2,9 +2,9 @@ import time
 import os
 
 
-def create_log_file1(file=None, prefix="", extension=".txt"):
+def create_log_file(file=None, prefix="", extension=".txt"):
     dir_name = "Logs"
-    sub_dir_name = dir_name + "\\" + time.strftime("%Y_%m_%d", time.localtime()) + " Лог СКЭ"
+    sub_dir_name = dir_name + "\\" + time.strftime("%Y_%m_%d", time.localtime()) + " Лог ADCS"
     try:
         os.makedirs(sub_dir_name)
     except (OSError, AttributeError) as error:
@@ -14,59 +14,24 @@ def create_log_file1(file=None, prefix="", extension=".txt"):
             file.close()
     except (OSError, NameError, AttributeError) as error:
         pass
-    file_name_1 = sub_dir_name + "\\" + time.strftime("%Y_%m_%d %H-%M-%S ",
-                                                    time.localtime()) + prefix + " СКЭ" + extension
+    file_name = sub_dir_name + "\\" + time.strftime("%Y_%m_%d %H-%M-%S ",
+                                                    time.localtime()) + prefix + " " + extension
 
-    return file_name_1, sub_dir_name
+    return file_name, sub_dir_name
 
-def create_log_file2(file=None, prefix="", extension=".txt"):
-    dir_name = "Logs"
-    sub_dir_name = dir_name + "\\" + time.strftime("%Y_%m_%d", time.localtime()) + " Лог СКЭ"
-    try:
-        os.makedirs(sub_dir_name)
-    except (OSError, AttributeError) as error:
-        pass
-    try:
-        if file:
+def open_log_file(file_name):
+    if file_name:
+        try:
+            file = open(file_name, 'a')
+        except (OSError, NameError, AttributeError) as error:
+            pass
+    pass
+    return file
+
+def close_log_file(file):
+    if file:
+        try:
             file.close()
-    except (OSError, NameError, AttributeError) as error:
-        pass
-    file_name_2 = sub_dir_name + "\\" + time.strftime("%Y_%m_%d %H-%M-%S ",
-                                                    time.localtime()) + prefix + " СКЭ" + extension
-
-    return file_name_2, sub_dir_name
-
-def open_log_file_1(file_name_1):
-    if file_name_1:
-        try:
-            file_1 = open(file_name_1, 'a')
-        except (OSError, NameError, AttributeError) as error:
-            pass
-    pass
-    return file_1
-
-def close_log_file_1(file_1):
-    if file_1:
-        try:
-            file_1.close()
-            print('close')
-        except (OSError, NameError, AttributeError) as error:
-            pass
-    pass
-
-def open_log_file_2(file_name_2):
-    if file_name_2:
-        try:
-            file_2 = open(file_name_2, 'a')
-        except (OSError, NameError, AttributeError) as error:
-            pass
-    pass
-    return file_2
-
-def close_log_file_2(file_2):
-    if file_2:
-        try:
-            file_2.close()
             #print('close')
         except (OSError, NameError, AttributeError) as error:
             pass
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import serial
 import struct
 
 import numpy.fft as fft
-
 import log_file
 
 ser = serial.Serial()
@@ -20,28 +19,48 @@ ser.flushOutput()  # очистка выходного буфера
 ser.flushInput()  # очистка входного буфера
 ser.reset_output_buffer()
 ser.reset_input_buffer()
+
 n = 0
-
 fig = plt.figure()
-
-#
-# file_handle_1, log_dir_1 = log_file.create_log_file1(prefix="kvv_log", extension=".txt")
-# file_1 = log_file.open_log_file_1(file_handle_1)
-# file_1.write("Time\tCommand Name\tCommand\tAnswer\n")
-# log_file.close_log_file_1(file_handle_1)
-
 
 
 def input_file_path():
     """Enter number of the selected channel; """
     channel = 0
-    input_str1 = input('Enter number of the selected channel [1-14]:', )
+    input_str1 = input('What channel is required? [0-13]:\n', )
     try:
         channel = input_str1
         print('Selected channel is', channel)
     except IndexError:
         print('Error!')
     return channel
+
+
+def logging_permission():
+    """Do you want to log data?"""
+    flag = 0
+    input_str = input('Do you want to log data?(y/n)\n', )
+    try:
+        if input_str == 'y':
+            flag = 1
+            print('Go logging')
+        elif input_str == 'n':
+            flag = 0
+            print('No logging')
+        else:
+            print('Error!')
+    except IndexError:
+        print('Error!')
+    return flag
+
+
+def logging(data_1=None, data_2=None):
+    file = log_file.open_log_file(file_handle)
+    i = 0
+    while i < 1024:
+        file.write("%0.f\n" % data_1[i])
+        i = i + 1
+    log_file.close_log_file(file_handle)
 
 
 def Bytes_to_NormBytes(data):
@@ -58,14 +77,14 @@ def Get_oscillogram(channel):
     i = 0
     Get_oscillogram_com = [0x41, int(channel)]
     ser.write(Get_oscillogram_com)
-    #print('\nGet_oscillogram_com is sended,\t', 'time =', time.strftime("%Y_%m_%d %H-%M-%S "))
+    # print('\nGet_oscillogram_com is sended,\t', 'time =', time.strftime("%Y_%m_%d %H-%M-%S "))
     output_data = ser.read(2048)
-    #print(Bytes_to_NormBytes(output_data))
+    # print(Bytes_to_NormBytes(output_data))
     while i < 2048:
         data_list.append(struct.unpack('<h', output_data[i:i + 2])[0])
         """points in ms; """
-        points.append(i*81.92/2048)
-        #print(i, Bytes_to_NormBytes(output_data)[i:i + 2], struct.unpack('<h', output_data[i:i + 2])[0])
+        points.append(i * 81.92 / 2048)
+        # print(i, Bytes_to_NormBytes(output_data)[i:i + 2], struct.unpack('<h', output_data[i:i + 2])[0])
         i = i + 2
     return data_list, points
 
@@ -78,17 +97,28 @@ def Plot(data_Y, data_X):
     ax.grid(True)
     ax.set_xlabel('time, ms')
     ax.set_ylabel('y-axis')
-    ax.set_xlim([0, 81.92])
-    ax.set_ylim([0, 4200])
-    #ax.scatter(data_X, data_Y, color='blue', marker='.')
-    plt.pause(0.05)
-    #plt.show()
+    ax.set_xlim([0, 81.92])  # 25kHz, 2048 points
+    ax.set_ylim([0, 4096])
+    # ax.scatter(data_X, data_Y, color='blue', marker='.')
+    plt.pause(0.0005)
+    # plt.show()
 
 
 Ch = input_file_path()
+flag_log = logging_permission()
+
+file_handle, log_dir = log_file.create_log_file(prefix="kvv_test", extension=".xls")
+
 while 1:
     data, Ppoints = Get_oscillogram(Ch)
     Plot(data, Ppoints)
+    while 5 < n < 10:
+        if flag_log == 1:
+            logging(data, Ppoints)
+            n = n + 1
+        else:
+            flag_log = 0
+            n = n + 1
     print(n, 'freq is')
     n = n + 1
 
